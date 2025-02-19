061bc9e8f2c2fa89132a1be1d9d0295b4703ed5b
Alathea FERRAND <alatheaferrand@gmail.com>
2025-02-19 12:03:08 -1000

    Split

--- air00.rb
+++ air00.rb
@@ -1,5 +1,5 @@
-# Split
-# Découpe une chaîne de caractères en tableau (séparateurs: espaces, tabulations, newlines)
+# Split : Découpe une chaîne de caractères en tableau
+# en utilisant des séparateurs : espaces, tabulations, newlines
 # Le programme devra utiliser une fonction prototypée comme ceci:
 # ma_fonction(string_à_couper, string_séparateur) {
 #  algorithme
@@ -7,13 +7,16 @@
 # }
 # Afficher error et quitter le programme en cas de problèmes d'arguments
 
-# Fonction
+# Fonction de découpe
 def decoupe(string_a_couper, string_separateur)
   tableau = []
   string_decoupe = ''
 
+  # Parcourt chaque caractère de la chaîne
   for i in (0...string_a_couper.length)
     est_separateur = false # Flag de séparation
+
+    # Vérifie si le caractère est un séparateur
     for j in (0...string_separateur.length)
       if string_a_couper[i] == string_separateur[j]
         est_separateur = true
@@ -21,35 +24,33 @@ def decoupe(string_a_couper, string_separateur)
       end
     end
     
+    # Ajoute le mot au tableau s'il n'est pas vide
     if est_separateur # true
-      if string_decoupe != ''
-        tableau << string_decoupe
-        string_decoupe = ''
-      end
+      tableau << string_decoupe unless string_decoupe.empty?
+      string_decoupe = ''
     else # flag false
       string_decoupe << string_a_couper[i]
     end
   end
 
-  if string_decoupe != '' # Ajoute le dernier mot
-    tableau << string_decoupe
-  end
+  # Ajoute le dernier mot s'il y en a un
+  tableau << string_decoupe unless string_decoupe.empty?
 
   return tableau
 end
 
-# Gestion d'erreurs
+# Gestion des erreurs
 if ARGV.length != 1 || ARGV[0].nil? || ARGV[0].empty?
   puts 'error'
   exit
 end
 
-# Parsing
+# Parsing des arguments
 string_a_couper = ARGV[0]
 string_separateur = [' ', "\t", "\n"]
 
 # Exécution
 tableau = decoupe(string_a_couper, string_separateur)
 
-# Affichage
+# Affichage du résultat
 puts tableau
